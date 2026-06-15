6817d741da2e004d5f1b424197268a1e589cfcdd
Heytor <heytorandrademedeiros2009@gmail.com>
2026-06-15 17:40:52 -0300

    resolvendo erro

--- src/types/session.d.ts
+++ src/types/session.d.ts
@@ -2,3 +2,15 @@ import "express-session";
 declare module "express-session" {
   interface SessionData { userId: number; userName: string; userRole: string; flash: string|null; }
 }
+import "express-session";
+
+declare module "express-session" {
+  interface SessionData {
+    flash?: string;
+    userId?: number;
+    userName?: string;
+    userRole?: string;
+  }
+}0
+
+export {};
